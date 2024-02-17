fd43f476a3d305adb8363df27b890074fb80bfe4
Amin <pahlavanmohammadamin@gmail.com>
2024-02-17 12:23:11 +0330

    movement is done

---
+++ app.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded' , () =>{
+  const bird = document.querySelector('.bird');
+  const gameDisplay = document.querySelector('.game-container');
+  const ground = document.querySelector('.ground');
+  let birdLeft = 220;
+  let birdBottom = 100;
+  let gravity = 2;
+
+  function startGame() {
+    birdBottom -= gravity;
+    bird.style.bottom = birdBottom + 'px';
+    bird.style.left = birdLeft + 'px';
+  }
+  let timerID = setInterval(startGame,20);
+
+  function control(e) {
+    console.log(e.keyCode)
+    if(e.keyCode === 38) {
+      jump();
+    }
+  }
+
+  function jump() {
+    if(birdBottom < 500) birdBottom += 50;
+    bird.style.bottom = birdBottom + 'px';
+  }
+
+  document.addEventListener('keyup', control)
+
+  function generateObstacle() {
+    let obstacleLeft = 500;
+    let randomHeight = Math.random() * 60;
+    let obstacleBottom = randomHeight;
+    const obstacle = document.createElement('div');
+    obstacle.classList.add('obstacle');
+    gameDisplay.appendChild(obstacle);
+    obstacle.style.left = obstacleLeft + 'px';
+    obstacle.style.bottom = obstacleBottom + 'px';
+  }
+  
+  generateObstacle()
+})
